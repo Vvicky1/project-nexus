d01a5e9f45e86baf9c6c63612320ce7db3f8cba7
Vvicky1 <157233470+Vvicky1@users.noreply.github.com>
2024-01-24 11:09:56 +0530

    Add files via upload

---
+++ intern 2/open.js
@@ -0,0 +1,9 @@
+function login() {
+    // Add logic for login functionality (not implemented in this example)
+    alert('Login functionality not implemented in this example.');
+}
+
+function signup() {
+    // Add logic for signup functionality (not implemented in this example)
+    alert('Signup functionality not implemented in this example.');
+}
